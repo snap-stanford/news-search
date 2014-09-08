66e126353c95464763ff18d73e98c10ff4feda13
Niko <niko.colneric@gmail.com>
2014-09-08 14:29:24 -0700

    re-added checing that all arguments are parsed. We do not need the main class name any more since the JAR is runnable

--- src/main/java/edu/stanford/snap/spinn3rHadoop/utils/ParseCLI.java
+++ src/main/java/edu/stanford/snap/spinn3rHadoop/utils/ParseCLI.java
@@ -179,10 +179,9 @@ public class ParseCLI {
 			cmd = parser.parse(options, args);
 			
 			/** If left unparsed arguments */
-			// TODO fix
-			//if(cmd.getArgs().length > 0){
-			//		throw new ParseException("Some extra arguments that were not parsed: " + cmd.getArgList());
-			//}
+			if(cmd.getArgs().length > 0){
+					throw new ParseException("Some extra arguments that were not parsed: " + cmd.getArgList());
+			}
 
 			/** Check date format and boundaries */
 			SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH");
