e93b61bebfa6198f7d5676d74a020d2582ae2917
Niko Colnerič <niko.colneric@gmail.com>
2014-09-17 17:18:54 -0700

    Disabling input fields when there is a file selected (JS)

---
+++ web-page/my/disable-input-when-file.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Niko on 15/09/14.
+ */
+
+$( document ).ready(function() {
+
+    /**
+     * Disable input fields when file is selected
+     * */
+    function disableFields(idPrefix){
+        return function(e) {
+            e.stopPropagation();
+            $("#"+idPrefix+"1").prop('disabled', true).get(0).value = "";
+            $("#"+idPrefix+"2").prop('disabled', true).get(0).value = "";
+            $("#"+idPrefix+"3").prop('disabled', true).get(0).value = "";
+            $("#"+idPrefix+"4").prop('disabled', true).get(0).value = "";
+            $("#"+idPrefix+"5").prop('disabled', true).get(0).value = "";
+        };
+    }
+
+    /**
+     * Re-enable input fields when file is removed
+     * */
+    function enableFields(idPrefix){
+        return function(e) {
+            e.stopPropagation();
+            $("#"+idPrefix+"1").prop('disabled', false);
+            $("#"+idPrefix+"2").prop('disabled', false);
+            $("#"+idPrefix+"3").prop('disabled', false);
+            $("#"+idPrefix+"4").prop('disabled', false);
+            $("#"+idPrefix+"5").prop('disabled', false);
+        };
+    }
+
+    var list = ["lang", "url", "keywords", "title", "content", "quotes"];
+    for(var id in list) {
+        var wl = list[id] + "W";
+        var bl = list[id] + "B";
+        $("#" + wl).on("change.bs.fileinput", disableFields(wl)).on("clear.bs.fileinput", enableFields(wl));
+        $("#" + bl).on("change.bs.fileinput", disableFields(bl)).on("clear.bs.fileinput", enableFields(bl));
+    }
+});
